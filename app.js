31157cc7d57eaf99a45ce77e1f8ff056bbfc919e
huy250901 <huyyy250901@gmail.com>
2023-02-19 20:53:18 +0700

    commit

---
+++ app.js
@@ -0,0 +1,69 @@
+// const $ = document.querySelector.bind(document)
+// const $$ = document.querySelectorAll.bind(document)
+
+//     const playlist = $('.playlist')
+
+// const songs = [
+//     {
+//         name: 'Anymore',
+//         singer: 'Charlie Puth',
+//         path: './assets/music/anymore.mp3',
+//         image: './assets/img/genshin5.png'
+//     },
+//     {
+//         name: 'CLOSE EYES',
+//         singer: 'DVRST',
+//         path: './assets/music/CLOSEEYES.mp3',
+//         image: './assets/img/genshin5.png'
+//     },
+//     {
+//         name: 'Dream Space',
+//         singer: 'DVRST',
+//         path: './assets/music/DreamSpace.mp3',
+//         image: './assets/img/genshin5.png'
+//     },
+//     {
+//         name: 'ENDLESS LOVE',
+//         singer: 'DVRST',
+//         path: './assets/music/ENDLESSLOVE.mp3',
+//         image: './assets/img/genshin5.png'
+//     },
+//     {
+//         name: 'Friendship',
+//         singer: 'Charlie Puth',
+//         path: './assets/music/Friendship.mp3',
+//         image: './assets/img/genshin5.png'
+//     },
+//     {
+//         name: 'IM SORRY',
+//         singer: 'Charlie Puth',
+//         path: './assets/music/IMSORRY.mp3',
+//         image: './assets/img/genshin5.png'
+//     },
+//     {
+//         name: 'Lucky',
+//         singer: 'Charlie Puth',
+//         path: './assets/music/Lucky.mp3',
+//         image: './assets/img/genshin5.png'
+//     },
+//     {
+//         name: 'Reality',
+//         singer: 'Charlie Puth',
+//         path: './assets/music/Reality.mp3',
+//         image: './assets/img/genshin5.png'
+//     },
+//     {
+//         name: 'RISE',
+//         singer: 'Charlie Puth',
+//         path: './assets/music/RISE.mp3',
+//         image: './assets/img/genshin5.png'
+//     },
+//     {
+//         name: 'Start Again',
+//         singer: 'Charlie Puth',
+//         path: './assets/music/StartAgain.mp3',
+//         image: './assets/img/genshin5.png'
+//     },
+    
+
+// ]
